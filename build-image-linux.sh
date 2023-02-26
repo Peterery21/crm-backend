b5662faf99e96904321b3b68345126942b802e81
Peterery21 <45755905+Peterery21@users.noreply.github.com>
2023-02-26 16:11:20 -0500

    Jenkinsfile

--- build-image-linux.sh
+++ build-image-linux.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-set -evx
-for dir in */; do
-    if [ -d "$dir" ]; then
-        echo "$dir"
-    fi
+SUB='service';
+containerName='peterado/tresosoft:';
+for dir in */;
+do
+  if ["$dir" == *"$SUB"*] ; then
+    echo "$dir"
+    mvn clean install -f "$dir"
+    docker stop "$dir"
+    docker rm "$dir"
+    docker rmi "$containerName""$dir"
+    docker buildx build "$dir" --platform linux/amd64 --push --tag="$containerName""$dir"
+    #docker build "$dir" --tag="$containerName""$dir"
+    #docker rmi $(docker images -a -q)
+  fi;
 done
-
